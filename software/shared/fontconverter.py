3ef15d89ffb92bf1d6d649ce88ecfff2731d077f
mike <barbeque@users.noreply.github.com>
2022-05-07 23:10:54 -0600

    The long byte-flipping nightmare is over

--- software/shared/fontconverter.py
+++ software/shared/fontconverter.py
@@ -13,6 +13,11 @@ for line in lines:
 # count to offset $20, then parse all the chunks
 start += 0x20
 
+def reverse_byte(byte):
+    binary = format(byte, '08b')
+    reversed_binary = binary[::-1]
+    return int(reversed_binary, base = 2)
+
 output = []
 for i in range(0, 128 - 0x20 - 1):
     # should be the format { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},   // U+0020 (space)
@@ -24,7 +29,16 @@ for i in range(0, 128 - 0x20 - 1):
     # remove trailing comma
     left, _, comment = line.rpartition('//')
     left, _, right = left.rpartition(',')
+
+    # split up the left side and flip the bytes (this is dumb)
+    left = ', '.join([hex(reverse_byte(int(t.replace('$',''), base=16))) for t in left.split(',')])
+
+    # add the comment back on
     line = left + '//' + comment
+
+    # each byte should be in reverse order
+
+
     # fix comments to assembler syntax
     line = line.replace('//', ';')
     # insert assembler declaration
