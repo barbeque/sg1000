e39ec4af793eb8e98e619fa9482b67a6159e1885
mike <barbeque@users.noreply.github.com>
2022-05-09 20:45:02 -0600

    1k test

--- software/ramtest/ramtest.asm
+++ software/ramtest/ramtest.asm
@@ -22,10 +22,58 @@ startup:
     ld hl, STR_URL
     call print_string
 
-    ; TODO: write the tester (16-bit index, probably)
+    ; begin the memory test - first 1k for check
+    ld hl, RAM_BASE
+    ld de, $0400 ; 1k
+_MemTest_Inner:
+    ld a, (hl)
+    cp a, $aa ; TODO: Use the RAM_TEST_VALUE_* and do this in a loop
+    jr z, _MemTest_MirrorDetected ; $aa was already in this byte
+
+    ; Write
+    ld a, $aa
+    ld (hl), a
+    nop
+    ; Read
+    ld a, (hl)
+    cp a, $aa
+    jr nz, _MemTest_ReadbackFailed ; We read back something that wasn't $aa
+
+_MemTest_Inner_End:
+    inc hl
+    dec de
+    ld a, d
+    or e
+    jr nz, _MemTest_Inner
+    
+    jr Test_Passed
+
     ; TODO: write an ASCII print routine (sub $20)
     ; TODO: write a routine to print out the failed address if any
-    ; TODO: detect mirroring
+
+_MemTest_ReadbackFailed:
+    ld b, 0
+    ld c, 2
+    ld hl, STR_READBACK_FAILED
+    call print_string
+    ; TODO: Print failed address HL
+    jr busy_loop
+
+_MemTest_MirrorDetected:
+    ; TODO: Print value of HL
+    ld b, 0
+    ld c, 2
+    ld hl, STR_MIRRORED
+    call print_string
+    ; TODO: Print failed address HL
+    jr busy_loop
+
+Test_Passed:
+    ld b, 0
+    ld c, 2
+    ld hl, STR_PASSED
+    call print_string
+    jr busy_loop
 
 busy_loop:
     jr busy_loop
@@ -57,6 +105,11 @@ _print_string_inner:
 
 STR_BANNER: .asciz "Soggy-1000 RAM tester"
 STR_URL: .asciz "https://www.leadedsolder.com"
+STR_MIRRORED: .asciz "Mirroring detected at:"
+STR_READBACK_FAILED: .asciz "Readback didn't match!"
+STR_PASSED: .asciz "Memory test passed."
+
+RAM_TEST_VALUES: .db $aa, $55, $cc, $00, $ff
 
 #include "../shared/vdp.asm"
 #include "../shared/font_8x8.asm"
