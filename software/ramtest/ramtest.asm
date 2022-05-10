9c13acb645b2bf1d1c2d350c0313ae024607ad40
mike <barbeque@users.noreply.github.com>
2022-05-09 21:15:30 -0600

    Hex printer

--- software/ramtest/ramtest.asm
+++ software/ramtest/ramtest.asm
@@ -52,6 +52,12 @@ _MemTest_Inner_End:
     ; TODO: write a routine to print out the failed address if any
 
 _MemTest_ReadbackFailed:
+    ; Print value of HL, which still should
+    ; be the last address tested
+    ld b, 1
+    ld c, 3
+    call print_hex
+    ; Print explanatory string
     ld b, 0
     ld c, 2
     ld hl, STR_READBACK_FAILED
@@ -60,7 +66,12 @@ _MemTest_ReadbackFailed:
     jr busy_loop
 
 _MemTest_MirrorDetected:
-    ; TODO: Print value of HL
+    ; Print value of HL
+    ld b, 1
+    ld c, 3
+    call print_hex
+
+    ; Print explanatory string
     ld b, 0
     ld c, 2
     ld hl, STR_MIRRORED
@@ -73,10 +84,89 @@ Test_Passed:
     ld c, 2
     ld hl, STR_PASSED
     call print_string
-    jr busy_loop
 
+    ; ok, let's try printing some test hexes
+    ld b, 1
+    ld c, 3
+    ld hl, $0101
+    call print_hex
+
+    jr busy_loop
 busy_loop:
     jr busy_loop
+
+get_hex_digit:
+    ; arguments:
+    ;   A - nibble (4-bit value, but we'll use the whole byte)
+    and $f ; n & 0xf
+    ld b, a
+    cp a, 10
+    jr c, _get_hex_digit_less_than_ten
+; greater than or equal to ten
+    ld a, $21 - 10 ; position of ascii 'A' in our font
+    jr _get_hex_digit_finish
+_get_hex_digit_less_than_ten:
+    ld a, $10 ; position of ascii '0' in our font
+    jr _get_hex_digit_finish
+_get_hex_digit_finish:
+    ;sub 10
+    add b
+    ret 
+
+print_hex:
+    ; arguments:
+    ;   B - X
+    ;   C - Y
+    ;   HL - value
+    push hl
+    calculate_write_address_from_xy
+    call SetVDPWriteAddress
+    pop hl
+
+    ; print the $ first, of course
+    ld a, $04
+    out (VDP_DATA), a
+#local
+_print_hex_inner:
+    ; thanks to https://chilliant.com/z80shift.html
+    ld de, hl
+    ; first digit: shift right 12
+    srl h
+    srl h
+    srl h
+    srl h
+    ld l, h
+    ld h, 0
+    ld a, l
+    call get_hex_digit
+    out (VDP_DATA), a
+    ld hl, de ; restore saved value
+    ; second digit: shift right 8
+    ld l, h
+    ld h, 0
+    ld a, l
+    call get_hex_digit
+    out (VDP_DATA), a
+    ld hl, de
+    ; third digit: shift right 4
+    srl h
+    rr l
+    srl h
+    rr l
+    srl h
+    rr l
+    srl h
+    rr l
+    ld a, l
+    call get_hex_digit
+    out (VDP_DATA), a
+    ; fourth digit: the remainder
+    ld hl, de
+    ld a, l
+    call get_hex_digit
+    out (VDP_DATA), a
+#endlocal
+    ret
 
 print_string:
     ; arguments:
