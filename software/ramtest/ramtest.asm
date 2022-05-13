7ce61e1dbe09746e648f25618ab17ad399cbdcf9
mike <barbeque@users.noreply.github.com>
2022-05-13 13:05:49 -0600

    PSG mute routine

--- software/ramtest/ramtest.asm
+++ software/ramtest/ramtest.asm
@@ -4,6 +4,8 @@
 
 #define TEST_LENGTH 2048 ; 2k - SG-1000 II
 
+#define PSG_REGISTERS $7f
+
 #include "../shared/vdp_macros.asm"
 
 #target rom
@@ -13,6 +15,7 @@ startup:
     call ScreenInit
     call DefineFont
     call InitFontPalette
+    call MutePSG
 
     ld b, 0
     ld c, 0
@@ -129,6 +132,17 @@ _get_hex_digit_finish:
     ;sub 10
     add b
     ret 
+
+MutePSG:
+    ld a, %10011111; channel 00: volume full mute
+    out (PSG_REGISTERS), a
+    ld a, %10111111; channel 01: volume full mute
+    out (PSG_REGISTERS), a
+    ld a, %11011111; channel 10: volume full mute
+    out (PSG_REGISTERS), a
+    ld a, %11111111; channel 11: volume full mute
+    out (PSG_REGISTERS), a
+    ret
 
 print_hex:
     ; arguments:
